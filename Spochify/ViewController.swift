5a147163bc44677c3f3754986bb83e0b292fc0a6
Alberto Penas Amor <alberto.penas.amor@mobgen.com>
2019-04-05 20:36:39 +0200

    Initial Commit

---
+++ Spochify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spochify
+//
+//  Created by Alberto on 05/04/2019.
+//  Copyright © 2019 com.github.albertopeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
